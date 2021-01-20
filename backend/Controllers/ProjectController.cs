1254cd4ad21eb403f5bf210dc1757e558b2fe36b
Chris Vietor <c.vietor@flynex.de>
2021-01-20 14:31:00 +0100

    added crud for projects

---
+++ backend/Controllers/ProjectController.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Models;
+using Firebase.Database;
+using Firebase.Database.Query;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProjectController : ControllerBase
+    {
+        private const string BaseUrl = "https://offline-data-sync-db-default-rtdb.europe-west1.firebasedatabase.app/";
+        
+        private readonly ILogger<ProjectController> _logger;
+
+        public ProjectController(ILogger<ProjectController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Project>> Get()
+        {
+            var firebaseClient = new FirebaseClient(BaseUrl);
+
+            var result = await firebaseClient
+                .Child("Projects")
+                .OnceAsync<Project>();
+
+            return (result.Select(item => {
+                item.Object.Id = item.Key;
+                return item.Object;
+            })).ToList();
+        }
+
+        [HttpPost]
+        public async Task<string> Post(Project project)
+        {
+            var firebaseClient = new FirebaseClient(BaseUrl);
+
+            var result = await firebaseClient
+                .Child("Projects")
+                .PostAsync<Project>(project);
+
+            return result.Key;
+        }
+
+        [HttpPatch]
+        public async Task Patch(Project project)
+        {
+            var firebaseClient = new FirebaseClient(BaseUrl);
+
+            await firebaseClient
+                .Child("Projects")
+                .Child(project.Id)
+                .PatchAsync(project);
+        }
+
+        [HttpDelete]
+        public async Task Delete(string key)
+        {
+            var firebaseClient = new FirebaseClient(BaseUrl);
+            
+            await firebaseClient
+                .Child("Projects")
+                .Child(key)
+                .DeleteAsync();
+        }
+    }
+}
---
+++ backend/Models/Project.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class Project
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
